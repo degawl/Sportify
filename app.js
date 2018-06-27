52ee6f9009eccf7bc676a8d64218e2f09e8eafc1
Devon Gawley <devongawley@gmail.com>
2018-06-27 13:24:35 +0000

    google maps autocomplete on create form

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes   = require("./routes/index");
     
 var url = process.env.DATABASEURL || "mongodb://localhost/sportify"
-mongoose.connect(url);
+mongoose.connect("mongodb://localhost/sportify");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
